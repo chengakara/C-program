df44c80f11e360199aa4969d2660ff0a4735d380
Mithhull Balaji <94139894+chengakara@users.noreply.github.com>
2022-08-27 12:15:21 +0530

    Create Recursion Concept.c
    concept of recursion delivered promptly

---
+++ Recursion Concept.c
@@ -0,0 +1,40 @@
+//RECURSION CONCEPT
+
+int main()
+{
+solve(3);
+return 0;
+}
+void solve(int x)
+{
+    if(x==0)
+{
+    printf("%d",x);
+    return;
+}
+printf("%d",x);
+solve(x-1);
+printf("%d",x);
+}
+
+JAGAN T M, 10:52 AM
+//RECURSION SAMPLE 2
+int g;
+main()
+{
+    test();
+}
+test()
+{
+    int l;
+    static int s;
+    l=++s;
+      ++g;
+    printf("%d %d %d \n",l,s,g);
+    if(l<=2)
+    
+{
+test();
+}
+printf("%d %d %d \n",l,s,g);
+}
